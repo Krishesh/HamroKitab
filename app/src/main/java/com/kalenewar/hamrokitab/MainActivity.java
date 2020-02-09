01debab32f162618d3eea1b0ac17b836e661be89
Krishesh <shrestha.krishesh@gmail.com>
2020-02-09 08:58:10 +0545

    login finished

--- app/src/main/java/com/kalenewar/hamrokitab/MainActivity.java
+++ app/src/main/java/com/kalenewar/hamrokitab/MainActivity.java
@@ -1,13 +1,31 @@
 package com.kalenewar.hamrokitab;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    MaterialButton  login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.material_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
